cff103b9c5b1ba52a7a0fb62b3c3243a088652bb
Gil Fernandes <gil.fernandes@onepointltd.com>
2023-11-25 13:10:24 +0000

    Removed github workflow

--- frontend/src/pages/Page.tsx
+++ frontend/src/pages/Page.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
-import { Alert, Box } from '@mui/material';
+import { Alert, Box, Stack } from '@mui/material';
 
 import Header from 'components/organisms/header';
 
@@ -13,6 +13,28 @@ import { userEnvState } from 'state/user';
 
 type Props = {
   children: JSX.Element;
+};
+
+const Banner = ({}) => {
+  return (
+    <Stack
+      alignItems="center"
+      direction="row"
+      className="image-banner"
+      alignContent="center"
+    >
+      <Stack
+        alignItems="center"
+        direction="column"
+        style={{ margin: '0 auto' }}
+      >
+        <h1>Data & Analytics Self-Assessment</h1>
+        <h2>
+          Powered by Onepoint's Data & Analytics Body of Knowledge and ChatGPT
+        </h2>
+      </Stack>
+    </Stack>
+  )
 };
 
 const Page = ({ children }: Props) => {
@@ -41,6 +63,7 @@ const Page = ({ children }: Props) => {
       }}
     >
       <Header />
+      <Banner />
       {!isAuthenticated ? (
         <Alert severity="error">You are not part of this project.</Alert>
       ) : (
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   "**/*.{js,jsx,ts,tsx}": ["npx prettier --write", "npx eslint --fix"],
   "**/*.{ts,tsx}": [() => "tsc --skipLibCheck --noEmit"],
-  "**/*.py": ["black", "isort --profile=black"],
+  // "**/*.py": ["black", "isort --profile=black"],
 };
